f0ea9cc94108451a0aad9c22d2d2def83b294a14
Davidsoumare25 <soumaredaouda784@gmail.com>
2026-02-13 09:53:46 +0000

    Delete storage.js

--- storage.js
+++
@@ -1,8 +0,0 @@
-export function saveResult(result) {
-  localStorage.setItem("disciplineData", JSON.stringify(result));
-}
-
-export function getLastResult() {
-  const data = localStorage.getItem("disciplineData");
-  return data ? JSON.parse(data) : null;
-}
